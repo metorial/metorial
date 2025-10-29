d25ed511e245fb85286d9644fb707886e31e6ba9
Tobias Herber <22559657+herber@users.noreply.github.com>
2025-10-29 13:19:00 -0700

    Add callbacks

--- packages/sdk/src/lib.ts
+++ packages/sdk/src/lib.ts
@@ -1,13 +1,14 @@
+import { setCallbackHandler } from './callbacks.ts';
 import { setOauthHandler } from './oauth.ts';
 import { createServer } from './server.ts';
 
 export * from '@modelcontextprotocol/sdk/server/index.js';
 export * from '@modelcontextprotocol/sdk/server/mcp.js';
-// export * from '@modelcontextprotocol/sdk/types.js';
 
 export * from 'zod';
 
 export let metorial = {
+  createServer,
   setOauthHandler,
-  createServer
+  setCallbackHandler
 };
