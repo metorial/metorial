07194d1b4c7c6bf08184ef6a87c27baf93dc5468
Ivan <vnxdeveloper@gmail.com>
2026-09-04 12:26:22 +0200

    Clean up

--- integrations/companies-house/src/tools.schema.test.ts
+++
@@ -1,4 +0,0 @@
-import { describeMcpCompatibleToolSchemas } from '@slates/test';
-import { provider } from './index';
-
-describeMcpCompatibleToolSchemas('Companies House tool input schemas', provider.actions);
