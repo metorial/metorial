3a9e26398f30fdfb2e46f7554922ba9518b2e245
Ivan <vnxdeveloper@gmail.com>
2026-09-04 12:59:34 +0200

    Clean up

--- integrations/destatis/vitest.config.ts
+++
@@ -1,7 +0,0 @@
-import { createSlatesVitestConfig } from '@slates/test/config';
-
-export default createSlatesVitestConfig({
-  test: {
-    include: ['src/**/*.test.ts', 'src/**/*.e2e.ts']
-  }
-});
